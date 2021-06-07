dbeddc94036fd51cb4001a4f3e31f565bbc3cd5f
Sai Teja N <saitejan@Sais-MacBook-Pro.local>
2021-06-08 02:13:41 +0530

    Company Details fix

--- app/src/main/java/com/example/placio/CompanyDetails.java
+++ app/src/main/java/com/example/placio/CompanyDetails.java
@@ -10,9 +10,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,12 +26,14 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class CompanyDetails extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
-    TextView name;
+    TextView name,location,Ctc,breakdown,date,offer,tenth,twefth,cur,clear,cgpa,batches,tier,desc;
     FirebaseFirestore firestore;
     Button confrim,cancel;
+    LinearLayout branLL,skillsLL,rolesLL;
 
 
     @Override
@@ -39,7 +43,27 @@ public class CompanyDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_details);
         name = findViewById(R.id.name);
+        location = findViewById(R.id.location);
+        Ctc = findViewById(R.id.ctc);
+        breakdown = findViewById(R.id.breakdown);
+        date = findViewById(R.id.date);
+        offer = findViewById(R.id.offering);
+        tenth = findViewById(R.id.marks10);
+        twefth = findViewById(R.id.marks12);
+        cur = findViewById(R.id.curback);
+        clear = findViewById(R.id.clback);
+        cgpa = findViewById(R.id.cgpa);
+//        batches = findViewById(R.id.batches);
+        tier = findViewById(R.id.tier);
+        desc = findViewById(R.id.desc);
+
+
+
         firestore = FirebaseFirestore.getInstance();
+        branLL=findViewById(R.id.BranchLL);
+        rolesLL=findViewById(R.id.PositionsLL);
+        skillsLL=findViewById(R.id.SkillsLL);
+
 
         String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DocumentReference docIdRef1 = firestore.collection("Companys").document(value);
@@ -49,8 +73,73 @@ public class CompanyDetails extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        String Name = document.get("Description").toString();
-                        name.setText(Name.toString());
+
+                        List<String> branch = (List<String>) document.get("Branch");
+                        List<String> batch = (List<String>) document.get("Batches");
+                        List<String> skills = (List<String>) document.get("Skills");
+                        List<String> role = (List<String>) document.get("Roles");
+
+
+//                        batches.setText(batch.toString());
+                        name.setText(document.get("Name").toString());
+                        desc.setText(document.get("Description").toString());
+                        location.setText(document.get("Location").toString());
+                        Ctc.setText(document.get("Ctc").toString()+"LPA");
+                        breakdown.setText(document.get("Breakdown").toString());
+                        date.setText("Last Day to apply"+document.get("Date").toString());
+                        offer.setText(document.get("Offer").toString());
+                        tenth.setText(document.get("Tenth").toString()+"%");
+                        twefth.setText(document.get("Twelfth").toString()+"%");
+                        cur.setText(document.get("Backlog").toString());
+                        clear.setText(document.get("ClBacklog").toString());
+                        cgpa.setText(document.get("Cgpa").toString());
+                        tier.setText(document.get("Tier").toString());
+
+
+                        final TextView[] branchitemView = new TextView[branch.size()];
+                        final TextView[] skillsitemView = new TextView[skills.size()];
+                        final TextView[] rolesitemView = new TextView[role.size()];
+
+
+                        for (int i = 0; i < branch.size(); i++) {
+                            final TextView branchitem = new TextView(getApplicationContext());
+                            branchitem.setText(branch.get(i));
+                            branchitem.setTextSize(11);
+                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                            params.setMargins(10,0,0,0);
+                            branchitem.setLayoutParams(params);
+                            branchitem.setWidth(100);
+                            branchitem.setGravity(Gravity.CENTER);
+                            branchitem.setBackgroundDrawable(getResources().getDrawable(R.drawable.details_items));
+                            branLL.addView(branchitem);
+                            branchitemView[i] = branchitem;
+                        }
+                        for (int i = 0; i < skills.size(); i++) {
+                            final TextView skillsitem = new TextView(getApplicationContext());
+                            skillsitem.setText(skills.get(i));
+                            skillsitem.setTextSize(11);
+                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                            params.setMargins(13,0,0,0);
+                            skillsitem.setLayoutParams(params);
+                            skillsitem.setPadding(8,8,8,8);
+                            skillsitem.setGravity(Gravity.CENTER);
+                            skillsitem.setBackgroundDrawable(getResources().getDrawable(R.drawable.details_items));
+                            skillsLL.addView(skillsitem);
+                            skillsitemView[i] = skillsitem;
+                        }
+                        for (int i = 0; i < role.size(); i++) {
+                            final TextView rolesitem= new TextView(getApplicationContext());
+                            rolesitem.setText(role.get(i));
+                            rolesitem.setTextSize(11);
+                            rolesitem.setPadding(8,8,8,8);
+                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                            params.setMargins(10,0,0,0);
+                            rolesitem.setLayoutParams(params);
+                            rolesitem.setGravity(Gravity.CENTER);
+                            rolesitem.setBackgroundDrawable(getResources().getDrawable(R.drawable.details_items));
+                            rolesLL.addView(rolesitem);
+                            rolesitemView[i] = rolesitem;
+                        }
                     } else {
                         Log.d("please", "help");
                     }
